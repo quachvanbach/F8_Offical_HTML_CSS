e1869fb04c2917d7925526136ebc6abd1afa33d3
quachvanbach <bachqv.net@gmail.com>
2022-06-10 15:50:46 +0700

    Update lession 25-28

--- 25.Constan_built_in/assets/js/main.js
+++ 25.Constan_built_in/assets/js/main.js
@@ -7,9 +7,9 @@ confirm('Hàm confirm. Xác nhận giá trị');
 prompt('Hàm prompt, điền và xác nhận giá trị nhập vào');
 
 setTimeout(function() {
-    console.log('Hàm console sẽ in ra giá trị này sau 3 giây', 3000)
-})
+    console.log('Hàm console sẽ in ra giá trị này sau 3 giây')}, 3000)
 
-setInterval(function() {
-    console.log('Hàm console sẽ in ra giá trị này liên tục sau mỗi 3 giây' + Math.random)
-}, 1000)
+
+setInterval(
+    console.log('Hàm console sẽ in ra giá trị này liên tục sau mỗi 3 giây' + Math.random())
+, 1000)
