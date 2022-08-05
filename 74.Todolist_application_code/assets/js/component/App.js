adff1e003f117b13d849b734b95ec7eef2b9b031
quachvanbach <bachqv.net@gmail.com>
2022-08-05 15:16:42 +0700

    Todolist app

---
+++ 74.Todolist_application_code/assets/js/component/App.js
@@ -0,0 +1,20 @@
+import html from "../core.js";
+import Header from "./Header.js";
+import TodoList from "./TodoList.js";
+import Footer from "./Footer.js";
+import { connect } from "../store.js";
+
+
+
+function App({ todos }) {
+    return html`
+    <section class="todoapp">
+        ${Header()}
+        ${todos.length > 0 && TodoList()}
+        ${todos.length > 0 && Footer()}
+    </section>
+    `
+}
+
+
+export default connect()(App);
